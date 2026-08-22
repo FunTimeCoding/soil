8e684aeb489fd6fd8dd552488ba4d59489d7c1e4
Alexander Reitzel <funtimecoding@gmail.com>
2026-08-22 02:11:35 +0200

    permit 100base-tx interfaces

--- pkg/netbox/constant/network.go
+++ pkg/netbox/constant/network.go
@@ -6,13 +6,15 @@ const (
 	Eth0 = "eth0"
 	Eth1 = "eth1"
 
-	InterfaceVirtual   = "virtual"
-	Interface1000BaseT = "1000base-t"
-	Interface2500BaseT = "2.5gbase-t"
+	InterfaceVirtual      = "virtual"
+	InterfaceFastEthernet = "100base-tx"
+	Interface1000BaseT    = "1000base-t"
+	Interface2500BaseT    = "2.5gbase-t"
 )
 
 var InterfaceTypes = []netbox.InterfaceTypeValue{
 	InterfaceVirtual,
+	InterfaceFastEthernet,
 	Interface1000BaseT,
 	Interface2500BaseT,
 }
